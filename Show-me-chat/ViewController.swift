f68ed68ae52aeebfd1e4e877f0ebe7ab4bfdeea1
DorofeevMark <creekcreek760@gmail.com>
2019-09-19 20:43:52 +0300

    Initial Commit

---
+++ Show-me-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Show-me-chat
+//
+//  Created by Mark Dorofeev on 19/09/2019.
+//  Copyright © 2019 Mark Dorofeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
