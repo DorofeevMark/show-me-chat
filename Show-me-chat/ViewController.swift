4f1a5fe6db333377207b3d4dd7f43a47c1855c21
Oleg <79211838196@yandex.ru>
2019-10-04 15:44:10 +0300

    Added card and Login screen

--- Show-me-chat/ViewController.swift
+++ Show-me-chat/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Show-me-chat
@@ -7,14 +8,19 @@
 //
 
 import UIKit
+import YandexMapKit
 
 class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var mapView: YMKMapView! //Yandex map
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.mapWindow.map.move(
+        with: YMKCameraPosition.init(target: YMKPoint(latitude: 59.9558213, longitude: 30.3209282), zoom: 15, azimuth: 0, tilt: 0),
+        animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
+        cameraCallback: nil)
     }
-
-
 }
-
