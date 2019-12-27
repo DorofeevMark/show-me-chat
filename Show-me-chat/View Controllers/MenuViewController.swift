f23aa2b02fb11bb5fec0c663f52c9abc50f379af
Oleg <79211838196@yandex.ru>
2019-12-27 11:22:31 +0300

    plus menu

---
+++ Show-me-chat/View Controllers/MenuViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MenuViewController.swift
+//  Show-me-chat
+//
+//  Created by 3oleg_krylov on 14.12.2019.
+//  Copyright © 2019 Mark Dorofeev. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        configureTableView()
+    }
+    
+    
+    func configureTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MenuTableCell.self, forCellReuseIdentifier: MenuTableCell.reuseId)
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+        
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 90
+        tableView.backgroundColor = .darkGray
+    }
+
+}
+
+extension MenuViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MenuTableCell.reuseId) as! MenuTableCell
+        let menuModel = MenuModel(rawValue: indexPath.row)
+        cell.iconImageView.image = menuModel?.image
+        cell.myLabel.text = menuModel?.description
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
