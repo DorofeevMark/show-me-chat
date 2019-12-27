49fc8d800d5743ffd8fcd26a9559de951501f981
Oleg <79211838196@yandex.ru>
2019-12-27 11:18:07 +0300

    Fixed2 menu

---
+++ Show-me-chat/Views/MenuTableSell.swift
@@ -0,0 +1,57 @@
+
+//
+//  MenuTableCell.swift
+//  Show-me-chat
+//
+//  Created by 3oleg_krylov on 24.12.2019.
+//  Copyright © 2019 Mark Dorofeev. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableCell: UITableViewCell {
+
+    static let reuseId = "MenuTableCell"
+    
+    let iconImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        
+        return iv
+    }()
+    
+    let myLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Кастомный текст"
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .white
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(iconImageView)
+        addSubview(myLabel)
+        
+        backgroundColor = .clear
+        
+        // iconImageView constaints
+        iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        iconImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        // myLabel constaints
+        myLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        myLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 12).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
